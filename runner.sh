551367e5cbc5aabf01641fe3ecf7795dd99b3f98
Saurav M. Hiremath <sauravhiremath@gmail.com>
2020-10-18 17:58:26 +0530

    update: add prod and dummy runner

--- runner.sh
+++ runner.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+export MONGO_DBNAME=example_db
+export MONGO_PASSWORD=example_password
+
 go build -o build/ .
 echo "[*] Build completed"
-./build/skeduler || echo "[x] SERVER INITIALISATION FAILED"
+
+./build/skeduler || sudo kill -9 $(sudo lsof -t -i:8080) || echo "[x] SERVER INITIALISATION FAILED"
 echo "[*] Server closed successfully"
