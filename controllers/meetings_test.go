bec843d6991c14e0eec8f2e15a8b8d2e99533913
Saurav M. Hiremath <sauravhiremath@gmail.com>
2020-10-19 12:55:14 +0530

    feat: add tests for getAllMeeting & forParticipant

--- controllers/meetings_test.go
+++ controllers/meetings_test.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+// TODO: Add mock mongoDB calls, currently used DB integration
 type MockDB struct {
 	meetings     []Meeting
 	participants []Participant
@@ -15,8 +16,8 @@ var mockDB = new(MockDB)
 func TestCreateMeeting(t *testing.T) {
 	startTime, _ := time.Parse(time.UTC.String(), "2019-10-18T10:44:56Z")
 	endTime, _ := time.Parse(time.UTC.String(), "2019-10-18T12:51:56Z")
-	m := Meeting{
-		Title: "Sample Meeting",
+	m1 := Meeting{
+		Title: "Sample Meeting 1",
 		Participants: []Participant{
 			{
 				Name:  "abc",
@@ -27,12 +28,47 @@ func TestCreateMeeting(t *testing.T) {
 		StartTime: startTime,
 		EndTime:   endTime,
 	}
-
-	got := CreateMeeting(m)
-
-	if got.Status != 200 {
-		t.Errorf("Got %q, expected %q", got.Status, 200)
+	m2 := Meeting{
+		Title: "Sample Meeting 2",
+		Participants: []Participant{
+			{
+				Name:  "xyz",
+				Email: "xyz@def.com",
+				RSVP:  "Yes",
+			},
+		},
+		StartTime: startTime,
+		EndTime:   endTime,
 	}
 
-	mockDB.meetings = append(mockDB.meetings, got.Data[0])
+	got1 := CreateMeeting(m1)
+	got2 := CreateMeeting(m2)
+
+	if got1.Status != 200 || got2.Status != 200 {
+		t.Errorf("Returned wrong Status Code. Got %q, expected %q", got1.Status, 200)
+	}
+
+	mockDB.meetings = append(mockDB.meetings, got1.Data[0])
+	mockDB.meetings = append(mockDB.meetings, got2.Data[0])
+}
+
+func TestGetAllMeetings(t *testing.T) {
+	startTime, _ := time.Parse(time.UTC.String(), "2019-10-18T10:24:56Z")
+	endTime, _ := time.Parse(time.UTC.String(), "2019-10-18T12:52:56Z")
+
+	got := GetAllMeetings(startTime, endTime)
+
+	if got.Status != 200 {
+		t.Errorf("Returned wrong Status Code. Got %q, expected %q", got1.Status, 200)
+	}
+}
+
+func TestGetMeetingForParticipant(t *testing.T) {
+	email := "abc@def.com"
+
+	got := GetMeetingForParticipant(email)
+
+	if got.Status != 200 {
+		t.Errorf("Returned wrong Status Code. Got %q, expected %q", got1.Status, 200)
+	}
 }
